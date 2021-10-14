8ff47ab90fd9dab66f6c9c155294ca62fa5b0fd6
0b5vr <0b5vr@0b5vr.com>
2021-10-15 01:21:33 +0900

    improve: use cache for transform

--- src/heck/Transform.ts
+++ src/heck/Transform.ts
@@ -1,73 +1,77 @@
+import { Cache } from '../utils/Cache';
 import { RawMatrix4, RawQuaternion, RawVector3, mat4Compose, mat4Decompose, mat4LookAt, mat4Multiply } from '@0b5vr/experimental';
 
 export class Transform {
-  protected __position: RawVector3;
+  protected __position: Cache<RawVector3>;
+  protected __rotation: Cache<RawQuaternion>;
+  protected __scale: Cache<RawVector3>;
+  protected __matrix: Cache<RawMatrix4>;
 
   public get position(): RawVector3 {
-    return this.__position;
+    return this.__position.value;
   }
 
   public set position( vector: RawVector3 ) {
-    this.__position = vector;
+    this.__position.value = vector;
 
-    this.__matrix = mat4Compose( this.__position, this.__rotation, this.__scale );
+    this.__matrix.needsUpdate = true;
     this.__isIdentity = false;
   }
 
-  protected __rotation: RawQuaternion;
 
   public get rotation(): RawQuaternion {
-    return this.__rotation;
+    return this.__rotation.value;
   }
 
   public set rotation( quaternion: RawQuaternion ) {
-    this.__rotation = quaternion;
+    this.__rotation.value = quaternion;
 
-    this.__matrix = mat4Compose( this.__position, this.__rotation, this.__scale );
+    this.__matrix.needsUpdate = true;
     this.__isIdentity = false;
   }
 
-  protected __scale: RawVector3;
 
   public get scale(): RawVector3 {
-    return this.__scale;
+    return this.__scale.value;
   }
 
   public set scale( vector: RawVector3 ) {
-    this.__scale = vector;
+    this.__scale.value = vector;
 
-    this.__matrix = mat4Compose( this.__position, this.__rotation, this.__scale );
+    this.__matrix.needsUpdate = true;
     this.__isIdentity = false;
   }
 
-  protected __matrix: RawMatrix4;
-
   public get matrix(): RawMatrix4 {
-    return this.__matrix;
+    return this.__matrix.value;
   }
 
   public set matrix( matrix: RawMatrix4 ) {
-    this.__matrix = matrix;
+    this.__matrix.value = matrix;
 
-    const { position, rotation, scale } = mat4Decompose( this.__matrix );
-    this.__position = position;
-    this.__rotation = rotation;
-    this.__scale = scale;
+    this.__position.needsUpdate = true;
+    this.__rotation.needsUpdate = true;
+    this.__scale.needsUpdate = true;
     this.__isIdentity = false;
   }
 
   protected __isIdentity: boolean;
 
   public constructor() {
-    this.__position = [ 0.0, 0.0, 0.0 ];
-    this.__rotation = [ 0.0, 0.0, 0.0, 1.0 ];
-    this.__scale = [ 1.0, 1.0, 1.0 ];
-    this.__matrix = [
-      1.0, 0.0, 0.0, 0.0,
-      0.0, 1.0, 0.0, 0.0,
-      0.0, 0.0, 1.0, 0.0,
-      0.0, 0.0, 0.0, 1.0,
-    ];
+    this.__position = new Cache( [ 0.0, 0.0, 0.0 ], () => this.__updateTRS().position );
+    this.__rotation = new Cache( [ 0.0, 0.0, 0.0, 1.0 ], () => this.__updateTRS().rotation );
+    this.__scale = new Cache( [ 1.0, 1.0, 1.0 ], () => this.__updateTRS().scale );
+
+    this.__matrix = new Cache(
+      [
+        1.0, 0.0, 0.0, 0.0,
+        0.0, 1.0, 0.0, 0.0,
+        0.0, 0.0, 1.0, 0.0,
+        0.0, 0.0, 0.0, 1.0,
+      ],
+      () => mat4Compose( this.position, this.rotation, this.scale ),
+    );
+
     this.__isIdentity = true;
   }
 
@@ -81,7 +85,15 @@ export class Transform {
     }
 
     const result = new Transform();
-    result.matrix = mat4Multiply( this.__matrix, transform.__matrix );
+    result.matrix = mat4Multiply( this.matrix, transform.matrix );
     return result;
+  }
+
+  private __updateTRS(): { position: RawVector3, rotation: RawQuaternion, scale: RawVector3 } {
+    const { position, rotation, scale } = mat4Decompose( this.matrix );
+    this.__position.value = position;
+    this.__rotation.value = rotation;
+    this.__scale.value = scale;
+    return { position, rotation, scale };
   }
 }
